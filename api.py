3b12b2ec1c4a5dccc1e7e1de6c5f245921de1c1e
jeffudacity <jeff.donovan@udacity.com>
2016-01-26 12:09:03 -0800

    Update API name for Hangman.

--- api.py
+++ api.py
@@ -20,9 +20,10 @@ USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
 
 MEMCACHE_MOVES_REMAINING = 'MOVES_REMAINING'
 
-@endpoints.api(name='guess_a_number', version='v1')
-class GuessANumberApi(remote.Service):
-    """Game API"""
+
+@endpoints.api(name='hangman', version='v1')
+class HangmanAPI(remote.Service):
+    """Hangman Game API"""
     @endpoints.method(request_message=USER_REQUEST,
                       response_message=StringMessage,
                       path='user',
@@ -146,4 +147,4 @@ class GuessANumberApi(remote.Service):
                          'The average moves remaining is {:.2f}'.format(average))
 
 
-api = endpoints.api_server([GuessANumberApi])
+api = endpoints.api_server([HangmanAPI])
