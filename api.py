17b787a83a54b04ddd4a65fecaa7ee2d963e4474
jeffudacity <jeff.donovan@udacity.com>
2016-01-26 12:23:57 -0800

    Update create_user endpoint for Hangman game.

--- api.py
+++ api.py
@@ -15,8 +15,11 @@ GET_GAME_REQUEST = endpoints.ResourceContainer(
 MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
     MakeMoveForm,
     urlsafe_game_key=messages.StringField(1),)
-USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
-                                           email=messages.StringField(2))
+
+USER_REQUEST = endpoints.ResourceContainer(
+    user_name=messages.StringField(1, required=True),
+    email=messages.StringField(2, required=False)
+    )
 
 MEMCACHE_MOVES_REMAINING = 'MOVES_REMAINING'
 
@@ -30,14 +33,15 @@ class HangmanAPI(remote.Service):
                       name='create_user',
                       http_method='POST')
     def create_user(self, request):
-        """Create a User. Requires a unique username"""
+        """Create a User - requires a unique username."""
         if User.query(User.name == request.user_name).get():
             raise endpoints.ConflictException(
                     'A User with that name already exists!')
         user = User(name=request.user_name, email=request.email)
         user.put()
-        return StringMessage(message='User {} created!'.format(
-                request.user_name))
+
+        success_message = 'User {} created!'.format(request.user_name)
+        return StringMessage(message=success_message)
 
     @endpoints.method(request_message=NEW_GAME_REQUEST,
                       response_message=GameForm,
